f313ebab8d964b7eda2b57ffd2438dcbde8668a0
soongu <naong526@naver.com>
2023-08-11 13:24:51 +0900

    useState 사용해보기

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [date, setDate] = useState('');
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const priceChangeHandler = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault(); // submit 차단
+    console.log('submit 버튼을 누름!');
+
+    const newExpense = {
+      title: title,
+      price: price,
+      date: date,
+    };
+    console.log(newExpense);
+
+    // 입력창 리셋
+    setTitle('');
+    setPrice('');
+    setDate('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={title}
+          />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
@@ -15,6 +53,8 @@ const ExpenseForm = () => {
             type="number"
             min="100"
             step="100"
+            onChange={priceChangeHandler}
+            value={price}
           />
         </div>
         <div className="new-expense__control">
@@ -23,6 +63,8 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2025-12-31"
+            onChange={dateChangeHandler}
+            value={date}
           />
         </div>
       </div>
